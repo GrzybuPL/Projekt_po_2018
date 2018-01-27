e5435b3b6b8acfb0ca7d7f4f063efa566f771c0b
Grzybu <Grzybu@DESKTOP-EPN1FT2>
2018-01-27 18:43:33 +0100

    \

--- Projekt_po_2018/GUI/gui.h
+++ Projekt_po_2018/GUI/gui.h
@@ -1,10 +1,10 @@
 #pragma once
-//projekt stworzony przez Dominik Grzybowski Marcin Zuber Micha³ Szyl Sebastian Przelios
+//projekt stworzony przez Dominik Grzybowski Marcin Zuber Micha³ Szyl Sebastian Przeliorz
 //
 
 
 
 
-void paraMain(int &argc, char** argv);
+void paraMain(int &argc, char** argv);//obsluga parametrow
 
-void loopProgram(const char* nazwa_ = "BiuroPodrozy.db");
+void loopProgram(const char* nazwa_ = "BiuroPodrozy.db");//glowna pendla programu

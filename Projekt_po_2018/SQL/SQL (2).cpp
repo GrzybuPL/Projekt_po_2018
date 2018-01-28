1415bc47154f48c0205a8959615843529e05bd9b
seba <sebci@gmail.om>
2018-01-28 14:17:45 +0100

    ss

--- Projekt_po_2018/SQL/SQL (2).cpp
+++ Projekt_po_2018/SQL/SQL (2).cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 
-SQL::SQL(const char* nazwaBazy_)
+/*SQL::SQL(const char* nazwaBazy_)
 {
 
 	if (sqlite3_open(nazwaBazy_, &db)) {
@@ -29,8 +29,72 @@ SQL::~SQL()
 	delete oferty;
 	delete promocje;
 	sqlite3_close(db);		//zamkniecie bazy danych
-}
+}*/
+void menuPromo(SQL * &baze)
+{
+	cout << "Menu" << endl;
+	cout << "Wybierz jedna z opcji co chcesz zrobic:" << endl;
+	cout << "1. Znajdz promocje" << endl;
+	cout << "2. Zatwierdz wybor promocji" << endl;
+	cout << "3. Zmien promocje" << endl;
+	cout << "0. exit" << endl;
 
+	while (open)
+	{
+		switch (_getch())
+		{
+		case '1':
+			;
+			break;
+		case '2':
+			;
+			break;
+		case '3':
+			;
+			break;
+			//...
+		case '0':
+			return false;
+			break;
+		default:
+			break;
+		}
+	}
+	return true;
+}
+void menuRezer(SQL * &baze)
+{
+	bool open = true;
+	cout << "Menu" << endl;
+	cout << "Wybierz jedna z opcji co chcesz zrobic:" << endl;
+	cout << "1. Dokonaj rezerwacji" << endl;
+	cout << "2. Zarzadzaj rezerwacjami" << endl;
+	cout << "3. Anuluj rezerwacje" << endl;
+	cout << "0. exit" << endl;
+
+	while (open)
+	{
+		switch (_getch())
+		{
+		case '1':
+			;
+			break;
+		case '2':
+			;
+			break;
+		case '3':
+			;
+			break;
+			//...
+		case '0':
+			return false;
+			break;
+		default:
+			break;
+		}
+	}
+	return true;
+}
 
 
 
